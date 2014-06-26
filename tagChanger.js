fc70101ef78d0a79344c2222fde610a796a765ed
Sazal <>
2014-06-26 10:10:57 +0545

    Added tagChanger.js, which is the js we use to replace contents of the 'tag' selection boxes

---
+++ tagChanger.js
@@ -0,0 +1,14 @@
+jsonObj={
+	"school":
+	{
+		"building_count":"No of buildings in school",
+		"isced:level":"Level"
+	},
+	"college":
+	{
+		"building_count":"No of buildings in college"
+	}
+}
+
+
+	
