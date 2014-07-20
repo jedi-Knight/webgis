88f416a61719c27ce0ac267c0817bf030d26865c
jediKnight <gautamxpratik@gmail.com>
2014-07-20 15:45:27 +0545

    geojson filename from client system time obtained from 'data' parameter of AJaX call;

--- GeoJSONwriter.php
+++ GeoJSONwriter.php
@@ -2,10 +2,11 @@
 	if(isset($_POST) && !empty($_POST)){
 	//if(isset($_POST)){
 		//$_POST['name'] = "file";
-		$date = (string)date("Y-m-d h:i:sa");
-		$strDate = str_replace(" ","-",$date);
-		$strDate = str_replace(":","-",$strDate);	//':' is NOT a valid character for a filename. So replace it
-		$myFile = $_POST['name'].$strDate.".geojson";
+//		$date = (string)date("Y-m-d h:i:sa");
+//		$strDate = str_replace(" ","-",$date);
+//		$strDate = str_replace(":","-",$strDate);	//':' is NOT a valid character for a filename. So replace it
+//		$myFile = $_POST['name'].$strDate.".geojson";
+            $myFile = $_POST['name'].$_POST['date'].".geojson";  //jedi-code
 		$fh = fopen($myFile, 'w') or die("can't open file");
 		$stringData = urldecode($_POST['payload']);
 		fwrite($fh, $stringData);
